2dd45b8dfb3b8d420c1be336714fa3eb4b45e772
kbshin <amazing1997@naver.com>
2024-03-10 15:42:10 +0900

    Initial Commit

---
+++ Miracle BrainUITests/Miracle_BrainUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Miracle_BrainUITestsLaunchTests.swift
+//  Miracle BrainUITests
+//
+//  Created by KIBEOM SHIN on 3/10/24.
+//
+
+import XCTest
+
+final class Miracle_BrainUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
